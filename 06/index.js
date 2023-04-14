abf15ea85a91b20cd6679337994f25ebe5df2ced
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-04-14 18:10:56 +0200

    modules

--- 06/index.js
+++ 06/index.js
@@ -8,3 +8,20 @@ filter(process.argv[2], process.argv[3], (error,data) => {
     console.log(data[i]);
   }
 });
+
+/*
+'use strict'
+    const filterFn = require('./solution_filter.js')
+    const dir = process.argv[2]
+    const filterStr = process.argv[3]
+
+    filterFn(dir, filterStr, function (err, list) {
+      if (err) {
+        return console.error('There was an error:', err)
+      }
+
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
+*/
--- 07/index.js
+++ 07/index.js
@@ -5,3 +5,14 @@ http.get(process.argv[2], (res) =>{
   res.on('data', console.log);
   res.on('error', console.error);
 });
+
+/*
+'use strict'
+  const http = require('http')
+
+  http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8')
+    response.on('data', console.log)
+    response.on('error', console.error)
+  }).on('error', console.error)
+*/
