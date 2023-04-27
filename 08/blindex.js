142be8449068f725c786af29b352b765de7f6660
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-04-27 17:09:11 +0200

    oggi

--- 08/blindex.js
+++ 08/blindex.js
@@ -11,3 +11,19 @@ http.get(process.argv[2], (res) =>{
     console.log(string);
   } ));
 });
+
+/*
+const http = require('http')
+   const bl = require('bl')
+
+   http.get(process.argv[2], function (response) {
+     response.pipe(bl(function (err, data) {
+       if (err) {
+         return console.error(err)
+       }
+       data = data.toString()
+       console.log(data.length)
+       console.log(data)
+     }))
+   })
+   */
