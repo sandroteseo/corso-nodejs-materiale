166b8440221355147bd802010f9f2465fbfce559
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-05-03 18:23:55 +0200

    today

--- 10/index.js
+++ 10/index.js
@@ -1,13 +1,45 @@
-var net = require('net');
-var strftime = require('strftime');
+const net = require('net');
+const port = process.argv[2];
 
 function getDate() {
-  // YYYY-MM-DD hh:mm
-  return strftime('%Y-%m-%d %H:%M');
+  let now = new Date();
+  return `${now.getFullYear()}-${fillZero(now.getMonth()+1)}-${fillZero(now.getDate())} ${fillZero(now.getHours())}:${fillZero(now.getMinutes())}`;
 }
 
-var server = net.createServer( (socket) =>{
-  socket.end(getDate()+"\n");
-} );
+function fillZero(value) {
+  if (value < 10)
+    return `0${value}`;
 
-server.listen(process.argv[2]);
+  return value;
+
+}
+
+const server = net.createServer(function (socket) {
+       // socket handling logic  now.getMinutes()
+       socket.write(getDate());
+       socket.end('\n');
+     });
+ server.listen(port);
+
+/*
+ const net = require('net')
+
+   function zeroFill (i) {
+     return (i < 10 ? '0' : '') + i
+   }
+
+   function now () {
+     const d = new Date()
+     return d.getFullYear() + '-' +
+       zeroFill(d.getMonth() + 1) + '-' +
+       zeroFill(d.getDate()) + ' ' +
+       zeroFill(d.getHours()) + ':' +
+       zeroFill(d.getMinutes())
+   }
+
+   const server = net.createServer(function (socket) {
+     socket.end(now() + '\n')
+   })
+
+   server.listen(Number(process.argv[2]))
+*/
