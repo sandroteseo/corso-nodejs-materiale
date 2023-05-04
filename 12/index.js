57159dd0f9e80b5582d92e13853ee9f52aa97e0b
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-05-04 17:17:46 +0200

    oggi

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,21 @@
+const http = require('http');
+const map = require('through2-map');
+const port = process.argv[2];
+
+
+const server = http.createServer( (req,res) =>{
+  if(req.method !== 'POST')
+    return res.end('Only POST please');
+
+  req.pipe(map( (chunk) => chunk.toString().toUpperCase() )).pipe(res);
+});
+
+server.listen(Number(port), ()=>{
+  console.log(`Server started: localhost:${port}`);
+});
+
+
+/*
 var http = require('http');
 var map = require('through2-map');
 
@@ -9,3 +27,4 @@ var server = http.createServer( (req,res) =>{
 });
 
 server.listen(Number(process.argv[2]));
+*/
