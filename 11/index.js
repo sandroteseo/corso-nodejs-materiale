8f419ffde536871fa3cb921ee2db68549b715cda
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-05-04 12:06:45 +0200

    Update index.js

--- 11/index.js
+++ 11/index.js
@@ -1,3 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const port = process.argv[2];
+const file = process.argv[3];
+
+const server = http.createServer( (req,res) =>{
+  console.log(req.url);
+  res.writeHead(200, { 'content-type' : 'text-plain' });
+
+  fs.createReadStream(file).pipe(res);
+});
+
+server.listen(Number(port), ()=>{
+  console.log(`Server started: localhost:${port}`);
+});
+
+
+/*
 var http = require('http');
 var fs = require('fs');
 
@@ -7,3 +25,4 @@ var server = http.createServer( (req,res) =>{
 });
 
 server.listen(Number(process.argv[2]));
+*/
